c90033c3248c772e1708a056127a9c47f078dfec
Maren Lilleberre <marenlilleberre@gmail.com>
2021-10-22 20:11:44 +0200

    add if statement to limit photos displayed

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ async function getSpace(){
    
     for (let i = 0; i < photos.length; i++) {
         const element = photos[i];
-       /*  if (i >1){
+//todo Add limit to photos shown?
+        if (i >10){
             break;
-        } */
+        }
 
-        //! Make a function to add html
-        //todo Add limit to photos shown?
-        //todo Make a function to choose "sol-date"
+       
+        
+        
         
         container.innerHTML+= `<div class="image" style="background-image: url('${element.img_src}')"></div>`
         
@@ -27,7 +28,7 @@ async function getSpace(){
     }
     
 }
-
+getSpace();
 /* rovers.addEventListener('change', (event) => {
     let result = event.target.value;
     console.log(result);
@@ -37,6 +38,8 @@ sol.addEventListener('change', (e) => {
     let result = e.target.value;
     console.log(result);
 }) */
+//todo Make a function to choose "sol-date"
+
 function searchSol() {
     let solDay = sol.value;
     let newUrl = new URL('https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h&')
@@ -57,7 +60,8 @@ function searchSol() {
 function findRover() {
     let rover = rovers.value;
     console.log(rover)
-    return rover;
+    //Todo Can i change the url to update rover?
+    /* return rover; */
     /* if (rover === "curiosity"){
         
     } else if (rover === 'spirit'){
@@ -67,12 +71,12 @@ function findRover() {
     } */
     
 }
-
+ //! Make a function to add html
 
 /* function createHTML(jsonResults){
 
 
 } */
-getSpace();
+
 
 
