26e4b94bc07bc82a6ed2d231d2556ee31c2e1f3b
Maren Lilleberre <marenlilleberre@gmail.com>
2021-10-29 02:58:07 +0200

    update querystring to add rover

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-let apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=100&api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h`
+let baseUrl =`https://api.nasa.gov/mars-photos/api/v1/`
+// let apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=100&api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h`
 const container = document.querySelector('.container');
 const loader = document.querySelector('.loader');
 const sol = document.getElementById('sol');
@@ -9,11 +10,11 @@ async function getSpace(){
     container.textContent = '';
     earthDayContainer.textContent = '';
     try{
-        const response = await fetch(apiUrl);
+        const response = await fetch(baseUrl);
         let jsonResults = await response.json();
         let photos= jsonResults.photos;
         console.log(photos);
-        earthDayContainer.innerHTML += `<p class="earth-day">Taken: ${photos[0].earth_date} by:  <a href=details.html?> ${photos[0].rover.name}</a></p>`
+        earthDayContainer.innerHTML += `<p class="earth-day">Taken: ${photos[0].earth_date} by:  <a href="rover.html?/${photos[0].rover.name.toLowerCase()}"> ${photos[0].rover.name}</a></p>`
     
         for (let i = 0; i < photos.length; i++) {
             const element = photos[i];
@@ -21,7 +22,7 @@ async function getSpace(){
             if (i >10){
                 break;
             }
-            container.innerHTML+= `<a href="details.html?id=${photos[i].id}" class="image" style="background-image: url('${element.img_src}')"></a>`
+            container.innerHTML+= `<a href="details.html?id=${element.id}&rover=${resultRover}" class="image" style="background-image: url('${element.img_src}')"></a>`
         }
     }catch(error) {
         container.innerHTML = `Error: ${error.message}`
@@ -32,24 +33,32 @@ async function getSpace(){
 //funskjon for å hente rover og redirekte til annen side
 //url med rovernavn->bytte navn og henge på info i querystringen(landingsdato, bilder tatt osv)
 //https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity?api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h
+let resultRover;
  rovers.addEventListener('change', (event) => {
      
-    let result = event.target.value;   
-    apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${result}/photos?sol=100&api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h`
+    resultRover = event.target.value;   
+    baseUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${resultRover}/photos?sol=100&api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h`
     getSpace();
-    
 })
 
-/* sol.addEventListener('change', (e) => {
+//todo Make a function to choose "sol-date"
+/* const solForm = document.querySelector('.sol-form');
+
+solForm.addEventListener('change', (e) => {
     let solDay = e.target.value;
+    let parsedSolInt = parseInt(solDay);
     let newUrl = new URL(`${apiUrl}`)
-    newUrl.searchParams.set('sol', `=${solDay}`)
-    getSpace();
-    
+    newUrl.searchParams.set('sol', `=${parsedSolInt}`)
+    console.log(newUrl)
+    getSpace();   
+})
+
+solForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
 }) */
 
 
-//todo Make a function to choose "sol-date"
+
 
  //! Make a function to add html
 
