5b6f7df4435d692543cae73e2008c84c8905a703
Maren Lilleberre <marenlilleberre@gmail.com>
2021-10-30 23:18:34 +0200

    add title and heading from api

--- js/details.js
+++ js/details.js
@@ -6,6 +6,10 @@ console.log(photoId)
 const rover = params.get('rover');
 const sol = params.get('sol')
 const apiKey = 'api_key=7C3efywnhiZulYlDVdUYW9M5u71tIS22nmcak61h'
+const title = document.querySelector('title')
+const heading = document.querySelector('h1');
+title.innerText=`${rover} photo`
+
 
 
 const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?id=${photoId}&sol=${sol}&${apiKey}`;
@@ -16,18 +20,25 @@ const container = document.querySelector('.photo-container')
 
 async function getDetails() {
     container.textContent = '';
-    const response = await fetch(apiUrl);
-    const result = await response.json();
-    let photo= result.photos;
-    for (let i = 0; i < photo.length; i++) {
-        const element = photo[i];
-        console.log(element)
-        if (element.id == photoId) {
-            displayPhoto = element.img_src;
-            console.log(element.id)
-            container.innerHTML = 
-            `<div class="card"><p>This picture is taken by ${element.rover.name} on ${element.earth_date}.<br> This image is shot with ${element.rover.name}s ${element.camera.full_name}. </p><div style="background-image: url('${displayPhoto}')"></div></div>`   
+    try{
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+        let photo = result.photos;
+        for (let i = 0; i < photo.length; i++) {
+            const element = photo[i];
+            console.log(result)
+            console.log(element)
+            if (element.id == photoId) {
+                displayPhoto = element.img_src;
+                console.log(element.id)
+                container.innerHTML = 
+                `<div class="card"><p>This picture was taken by ${element.rover.name} on ${element.earth_date}.<br> This image was shot with ${element.rover.name}s ${element.camera.full_name}. </p><div style="background-image: url('${displayPhoto}')"></div></div>`   
+                heading.innerText =`Mars ${element.rover.name} Photos`
+            }
         }
+    }catch(error){
+        container.innerHTML= `Something went wrong: ${error.message}`
+
     }
 }
 getDetails();
